8bfa820c2e6c7babea570cfe33388edf629f58ff
KJ J <Kristjanjohann@gmail.com>
2018-09-18 13:02:53 +0000

    Changes to be committed: 	modified:   TileTraveller.py

--- TileTraveller.py
+++ TileTraveller.py
@@ -13,26 +13,26 @@ def prompt(a):
     print("You can travel: ",a)
 def winner():
     print("Victory")
-# def new_direction(a,b):
-#     if a == "1,1":
-#         b == "(N)orth"
-#     elif a == "1,2":
-#        b == "(N)orth" +" or "+ "(S)outh" +" or "+ "(E)ast"
-#     elif a == "1,3":
-#        b == "(S)outh" +" or "+"(E)ast"
-#     elif a =="2,1":
-#         b == "(N)orth"
-#     elif a =="2,2":
-#        b == "(S)outh"+" or "+"(W)est"
-#     elif a =="2,3":
-#         b =="(E)ast"+" or "+"(W)est"
-#     elif a =="3,1":
-#        b == "(N)orth"
-#     elif a =="3,2":
-#         b == "(N)orth" +" or "+ "(S)outh"
-#     elif a =="3,3":
-#         b == (W)est"+" or "+"(S)outh"
-#     return b
+def new_direction(a,b):
+    if a == "1,1":
+        b = "(N)orth"
+    elif a == "1,2":
+       b = "(N)orth" +" or "+ "(S)outh" +" or "+ "(E)ast"
+    elif a == "1,3":
+       b = "(S)outh" +" or "+"(E)ast"
+    elif a =="2,1":
+        b = "(N)orth"
+    elif a =="2,2":
+       b = "(S)outh"+" or "+"(W)est"
+    elif a =="2,3":
+        b ="(E)ast"+" or "+"(W)est"
+    elif a =="3,1":
+       b = "(N)orth"
+    elif a =="3,2":
+        b = "(N)orth" +" or "+ "(S)outh"
+    elif a =="3,3":
+        b = "(W)est"+" or "+"(S)outh"
+    return b
 
 
 
@@ -45,28 +45,12 @@ a = 0
 b = 0
 
 #The walls
+possible_direction = ""
 
-if current_location == "1,1":
-    possible_direction = "(N)orth"
-elif current_location == "1,2":
-    possible_direction = "(N)orth" +" or "+ "(S)outh" +" or "+ "(E)ast"
-elif current_location == "1,3":
-    possible_direction = "(S)outh" +" or "+"(E)ast"
-elif current_location =="2,1":
-    possible_direction = "(N)orth"
-elif current_location =="2,2":
-    possible_direction = "(S)outh"+" or "+"(W)est"
-elif current_location =="2,3":
-    possible_direction ="(E)ast"+" or "+"(W)est"
-elif current_location =="3,1":
-    possible_direction = "(N)orth"
-elif current_location =="3,2":
-    possible_direction = "(N)orth" +" or "+ "(S)outh"
-elif current_location =="3,3":
-    possible_direction = "(W)est"+" or "+"(S)outh"
+possible_direction = new_direction(current_location,possible_direction)
 
 prompt(possible_direction)
-user_input = input("Input a direction: ")
+user_input = input("Direction: ")
 
 direction = str(user_input)
 
@@ -76,113 +60,32 @@ east = "Ee"
 west = "Ww"
 
 
-
-
-
 #Player interaction
 
 while current_location_x > 0 and current_location_x <= 3 and current_location_y > 0 and current_location_y <= 3:
     if direction in north and "(N)orth" in possible_direction:
         current_location = str(current_location_x) +","+str(directionup(a,current_location_y))
-        print(current_location)
-        current_location_y += 1  
-        if current_location == "1,1":
-            possible_direction = "(N)orth"
-        elif current_location == "1,2":
-            possible_direction = "(N)orth" +" or "+ "(S)outh" +" or "+ "(E)ast"
-        elif current_location == "1,3":
-            possible_direction = "(S)outh" +" or "+"(E)ast"
-        elif current_location =="2,1":
-            possible_direction = "(N)orth"
-        elif current_location =="2,2":
-            possible_direction = "(S)outh"+" or "+"(W)est"
-        elif current_location =="2,3":
-            possible_direction ="(E)ast"+" or "+"(W)est"
-        elif current_location =="3,1":
-            possible_direction = "(N)orth"
-        elif current_location =="3,2":
-            possible_direction = "(N)orth" +" or "+ "(S)outh"
-        elif current_location =="3,3":
-            possible_direction = "(W)est"+" or "+"(S)outh"
+        current_location_y += 1
+        possible_direction = new_direction(current_location,possible_direction)
     elif direction in south and "(S)outh" in possible_direction:
         current_location = str(current_location_x)+","+str(directiondown(a,current_location_y))
-        print(current_location)
         current_location_y -= 1
-        if current_location == "1,1":
-            possible_direction = "(N)orth"
-        elif current_location == "1,2":
-            possible_direction = "(N)orth" +" or "+ "(S)outh" +" or "+ "(E)ast"
-        elif current_location == "1,3":
-            possible_direction = "(S)outh" +" or "+"(E)ast"
-        elif current_location =="2,1":
-            possible_direction = "(N)orth"
-        elif current_location =="2,2":
-            possible_direction = "(S)outh"+" or "+"(W)est"
-        elif current_location =="2,3":
-            possible_direction ="(E)ast"+" or "+"(W)est"
-        elif current_location =="3,1":
-            possible_direction = "(N)orth"
-        elif current_location =="3,2":
-            possible_direction = "(N)orth" +" or "+ "(S)outh"
-        elif current_location =="3,3":
-            possible_direction = "(E)ast"+" or "+"(S)outh"
+        possible_direction = new_direction(current_location,possible_direction)
     elif direction in east and "(E)ast" in possible_direction:
         current_location = str(directionright(current_location_x,b))+","+str(current_location_y)
-        print(current_location)
         current_location_x += 1
-        if current_location == "1,1":
-            possible_direction = "(N)orth"
-        elif current_location == "1,2":
-            possible_direction = "(N)orth" +" or "+ "(S)outh" +" or "+ "(E)ast"
-        elif current_location == "1,3":
-            possible_direction = "(S)outh" +" or "+"(E)ast"
-        elif current_location =="2,1":
-            possible_direction = "(N)orth"
-        elif current_location =="2,2":
-            possible_direction = "(S)outh"+" or "+"(W)est"
-        elif current_location =="2,3":
-            possible_direction ="(E)ast"+" or "+"(W)est"
-        elif current_location =="3,1":
-            possible_direction = "(N)orth"
-        elif current_location =="3,2":
-            possible_direction = "(N)orth" +" or "+ "(S)outh"
-        elif current_location =="3,3":
-            possible_direction = "(W)est"+" or "+"(S)outh"
+        possible_direction = new_direction(current_location,possible_direction)
     elif direction in west and "(W)est" in possible_direction:
         current_location = str(directionleft(current_location_x,b))+","+str(current_location_y)
         current_location_x -= 1
-        print(current_location)
-        if current_location == "1,1":
-            possible_direction = "(N)orth"
-        elif current_location == "1,2":
-            possible_direction = "(N)orth" +" or "+ "(S)outh" +" or "+ "(E)ast"
-        elif current_location == "1,3":
-            possible_direction = "(S)outh" +" or "+"(E)ast"
-        elif current_location =="2,1":
-            possible_direction = "(N)orth"
-        elif current_location =="2,2":
-            possible_direction = "(S)outh"+" or "+"(W)est"
-        elif current_location =="2,3":
-            possible_direction ="(E)ast"+" or "+"(W)est"
-        elif current_location =="3,1":
-            possible_direction = "(N)orth"
-        elif current_location =="3,2":
-            possible_direction = "(N)orth" +" or "+ "(S)outh"
-        elif current_location =="3,3":
-            possible_direction = "(W)est"+" or "+"(S)outh"
+        possible_direction = new_direction(current_location,possible_direction)
     else:
         print("Invalid direction, try again") 
     if current_location == "3,1":
         winner()
         break
-    print(current_location)
     prompt(possible_direction)
-    direction = input("Input a direction: ")
-
-        
-
-# elif current_location == "1,1" and direction != north:
-#     print("Direction is invalid, try again")
+    direction = input("Direction: ")
 
     
 
